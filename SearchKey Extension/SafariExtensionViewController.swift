2a275e24a5015a9a47a24277555943728d741190
jerome <jerome@whatever.com>
2021-04-07 20:53:33 -0500

    Initial Commit

---
+++ SearchKey Extension/SafariExtensionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SafariExtensionViewController.swift
+//  SearchKey Extension
+//
+//  Created by jerome on 4/7/21.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared: SafariExtensionViewController = {
+        let shared = SafariExtensionViewController()
+        shared.preferredContentSize = NSSize(width:320, height:240)
+        return shared
+    }()
+
+}
